2339a68cbffce46af00f1a59752e80b21ffeb5c1
Matthew Loyd <mstarrloyd@gmail.com>
2023-12-08 14:03:06 -0500

    Set up initial files

---
+++ app.py
@@ -0,0 +1,12 @@
+import numpy as np
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template('home.html')
+
+@app.route("/test/")
+def rest():
+	return render_template('test.html')
