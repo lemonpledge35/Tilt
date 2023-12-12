2a40aec675bd3f6c6d65498d68f1e92bfb5a219e
Matthew Loyd <mstarrloyd@gmail.com>
2023-12-11 19:55:39 -0500

    Added html from chatgpt and tweaked it to respond to button presses

--- app.py
+++ app.py
@@ -1,13 +1,62 @@
 import numpy as np
 from flask import Flask, render_template, request
 import datetime
+import threading
+
+global brewing
+brewing = False
+
+
+class Brew():
+    def __init__(self,name):
+        self.name = name
+        
+def get_values():
+    t = 75
+    sg = 1004
+    return t,sg
+
+def begin_brew(fn):
+    global brewing
+    brewing = True
+    beer = Brew(fn)
+    return beer
+
+def end_brew():
+    global brewing
+    brewing = False
+    
+
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ['GET','POST'])
 def home():
-	t = datetime.datetime.now().strftime("%H:%M:%S")
-	return render_template('home.html',current_time=t)
+    global brewing
+    t,s = get_values()
+    #t = datetime.datetime.now().strftime("%H:%M:%S")
+    if request.method == 'POST':
+        if 'beginbrew' in request.form:
+            fn = request.form['beerName']
+            beer = begin_brew(fn)
+        elif 'endbrew' in request.form:
+            end_brew()
+
+        get_values()
+        t,s = get_values()
+        if brewing:
+            bs = f'Brewing {beer.name}'
+        else:
+            bs = 'Not Brewing'
+        return render_template('home.html',temp=t,sg=s,brewing_status=bs)
+    
+    if request.method == 'GET':
+        if brewing:
+            return render_template('home.html',temp=t,sg = s,brewing_status = 'Brewing')
+        else:
+            return render_template('home.html',temp=t,sg=s,brewing_status='Not Brewing')
+    
+
 
 @app.route("/test/")
 def rest():
